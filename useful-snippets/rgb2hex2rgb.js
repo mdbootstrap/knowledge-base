aeebbfca90a2e31462925aeab874f27ffd658a65
Dawid Adach <d.adach@mdbootstrap.com>
2020-12-02 13:03:46 +0100

    Addding missing files content

---
+++ useful-snippets/rgb2hex2rgb.js
@@ -0,0 +1,25 @@
+function rgbToHex(r, g, b) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+// rgbToHex(0, 51, 255); // #0033ff
+
+function hexToRgb(hex) {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+}
+
+// hexToRgb("#0033ff").g;   // "51";
+// hexToRgb("#03f").g;      // "51";
--- useful-snippets/uniqueIDgenerator.js
+++ useful-snippets/uniqueIDgenerator.js
@@ -31,3 +31,12 @@ const uniqueAlphaNumericId = (() => {
   return (length = 24) =>
     Array.from({ length }, () => heyStack[randomInt()]).join("");
 })();
+
+//UUID
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
